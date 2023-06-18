32f58b958515803f1aaf199d00648cc431466da1
Neil <homzzr@gmail.com>
2023-06-18 21:30:00 +0300

    Создал network client

---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct NetworkClient {
+
+    private enum NetworkError: Error {                                                      // сетевая ошибка
+        case codeError
+    }
+    
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {                // функция запроса
+        let request = URLRequest(url: url)                                                  // создаем запрос
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Проверяем, пришла ли ошибка
+            if let error = error {                                                          // распаковываем ошибку
+                handler(.failure(error))
+                return
+            }
+            
+            // Проверяем, что нам пришёл успешный код ответа
+            if let response = response as? HTTPURLResponse,                                 // обрабатываем код ответа
+                response.statusCode < 200 || response.statusCode >= 300 {
+                handler(.failure(NetworkError.codeError))
+                return
+            }
+            
+            // Возвращаем данные
+            guard let data = data else { return }                                           // обрабатываем успешный ответ
+            handler(.success(data))
+        }
+        
+        task.resume()
+    }
+}
