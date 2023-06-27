e46e87e74ed5a03be804b1fe73c68d4920d844f2
Neil <homzzr@gmail.com>
2023-06-27 19:39:06 +0300

    Перенес функцию конвертации

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -1,1 +1,15 @@
 import UIKit
+
+final class MovieQuizPresenter {
+    
+    private var currentQuestionIndex: Int = 0
+    private let questionsAmount: Int = 10
+    
+    func convert(model:QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(data: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
+        return questionStep
+    }
+}
